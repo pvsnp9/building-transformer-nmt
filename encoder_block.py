encoder masking fixed

encoder_block.py
@@ -2,11 +2,12 @@ import torch
 import torch.nn as nn 
 from multihead_attention import MultiHeadAttention
 from residual_connection import ResidualConnection
+from feedforward import FeedForward
 from layernorm import LayerNormalization
 
 
 class EncoderBlock(nn.Module):
-    def __init__(self, features, attention_block, ff_block, dropout) -> None:
+    def __init__(self, features:int, attention_block:MultiHeadAttention, ff_block:FeedForward, dropout:float) -> None:
         super().__init__()
         
         self.attention_block = attention_block

multihead_attention.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import math
 
 class MultiHeadAttention(nn.Module):
-    def __init__(self, d_model, h, dropout):
+    def __init__(self, d_model:int, h:int, dropout:float):
         super().__init__()
         self.d_model = d_model
         self.h = h
@@ -19,13 +19,13 @@ class MultiHeadAttention(nn.Module):
         self.dropout = nn.Dropout(dropout)
     
     @staticmethod
-    def attention(q, k, v, mask, dropout):
+    def attention(q, k, v, mask, dropout:nn.Dropout):
         d_k = q.shape[-1]
         
         #(bathc, h, seq_len, d_k) -> (bathc, h, seq_len, seq_len)
         attention_scores = (q @ k.transpose(-2, -1)) / math.sqrt(d_k)
         if mask is not None:
-            attention_scores.masked_fill(mask==0, -1e9)
+            attention_scores.masked_fill_(mask==0, -1e9)
         #(batch, h, seq_len, seq_len)
         attention_scores = attention_scores.softmax(dim=-1)
         if dropout is not None:

train.py
@@ -73,12 +73,12 @@ def get_dataset(cfg):
 
 
 def get_model(cfg, vocab_src_len, vocab_target_len):
-    model = build_transformer(vocab_src_len, vocab_target_len, cfg["seq_len"], cfg["seq_len"],d_model=cfg['d_model'])
+    model = build_transformer(vocab_src_len, vocab_target_len, cfg["seq_len"], cfg["seq_len"],d_model=cfg['d_model'],N=cfg['num_layers'], h=cfg["num_heads"])
     return model
 
 def train_model(cfg):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    if (device == 'cuda'):
+    if (device.type == 'cuda'):
         print(f"Device name: {torch.cuda.get_device_name(device.index)}")
         print(f"Device memory: {torch.cuda.get_device_properties(device.index).total_memory / 1024 ** 3} GB")
     
@@ -186,7 +186,7 @@ def eval_model(model, val_ds, target_tokenizer, max_len, device, print_msg, num_
 
             assert encoder_input.size(0) == 1, "Validation must have single batch"
             
-            model_out = greedy_decode(model, encoder_input, encoder_mask, target_tokenizer, max_len, device)
+            model_out =  greedy_decode(model, encoder_input, encoder_mask, target_tokenizer, max_len, device)
             
             src_txt = batch["src_txt"][0]
             tgt_txt = batch["tgt_txt"][0]